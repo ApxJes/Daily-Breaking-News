2a93d49591a183b2b06a94cf42f161b564d16b59
aungpyaekyaw <shwyzee109@gmail.com>
2025-01-25 14:38:19 +0630

    display breaking news on breakingNews fragment

--- app/src/main/java/com/example/retrofittutorial/ui/ui/NewsActivity.kt
+++ app/src/main/java/com/example/retrofittutorial/ui/ui/NewsActivity.kt
@@ -7,17 +7,28 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.retrofittutorial.R
 import com.example.retrofittutorial.databinding.ActivityNewsBinding
+import com.example.retrofittutorial.ui.adapter.NewsAdapter
+import com.example.retrofittutorial.ui.db.ArticleDatabase
+import com.example.retrofittutorial.ui.repository.NewsRepository
+import com.example.retrofittutorial.ui.viewModel.NewsViewModel
+import com.example.retrofittutorial.ui.viewModel.NewsViewModelFactory
 
 
 class NewsActivity : AppCompatActivity() {
 
     private var _binding: ActivityNewsBinding? = null
     private val binding get() = _binding!!
+    lateinit var newsViewModel: NewsViewModel
+    private lateinit var newsAdapter: NewsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityNewsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val newsViewModelFactory = NewsViewModelFactory(application, newsRepository)
+        newsViewModel = ViewModelProvider(this, newsViewModelFactory).get(NewsViewModel::class.java)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
--- app/src/main/java/com/example/retrofittutorial/ui/ui/fragment/BreakingNewsFragment.kt
+++ app/src/main/java/com/example/retrofittutorial/ui/ui/fragment/BreakingNewsFragment.kt
@@ -6,9 +6,56 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.retrofittutorial.R
 import com.example.retrofittutorial.databinding.FragmentBreakingNewsBinding
+import com.example.retrofittutorial.ui.adapter.NewsAdapter
 import com.example.retrofittutorial.ui.ui.NewsActivity
+import com.example.retrofittutorial.ui.util.Resource
+import com.example.retrofittutorial.ui.viewModel.NewsViewModel
 import com.google.android.material.snackbar.Snackbar
 
 class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
+    private lateinit var binding: FragmentBreakingNewsBinding
+    private lateinit var newsViewModel: NewsViewModel
+    private lateinit var newsAdapter: NewsAdapter
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentBreakingNewsBinding.bind(view)
+        newsViewModel = (activity as NewsActivity).newsViewModel
+
+        newsAdapter = NewsAdapter()
+        binding.rcvBreakingNews.adapter = newsAdapter
+        binding.rcvBreakingNews.layoutManager = LinearLayoutManager(activity)
+
+        newsViewModel.breakingNews.observe(viewLifecycleOwner) {respone ->
+            when(respone) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    respone.data?.let { resultResponse ->
+                        newsAdapter.differ.submitList(resultResponse.articles)
+                    }
+                }
+
+                is Resource.Error -> {
+                    hideProgressBar()
+                    respone.data?.let { errorMessage ->
+                        Snackbar.make(
+                            view,
+                            "An error occur $errorMessage",
+                            Snackbar.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+
+                is Resource.Loading -> showProgressBar()
+            }
+        }
+    }
+
+    private fun showProgressBar() {
+        binding.paginationProgress.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        binding.paginationProgress.visibility = View.GONE
+    }
 }
